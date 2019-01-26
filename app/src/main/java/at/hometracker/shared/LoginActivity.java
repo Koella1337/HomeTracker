31e15522e21c31848ca59ff4f32d45b5f88e7d33
Koella1337 <endtmayer.alexander@gmx.at>
2019-01-26 11:34:59 +0100

    Created basic LoginActivity

---
+++ app/src/main/java/at/hometracker/shared/LoginActivity.java
@@ -0,0 +1,20 @@
+package at.hometracker.shared;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import at.hometracker.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    private void login(String username, String password) {
+
+    }
+
+}
--- app/src/main/java/at/hometracker/shared/Utils.java
+++ app/src/main/java/at/hometracker/shared/Utils.java
@@ -1,9 +1,13 @@
 package at.hometracker.shared;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
+import static android.content.Context.MODE_PRIVATE;
 import static android.support.v4.app.ActivityCompat.startActivityForResult;
+import static at.hometracker.shared.Constants.*;
 
 public class Utils {
 
@@ -12,7 +16,14 @@ public class Utils {
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("image/*");
         intent = Intent.createChooser(intent, "Select Picture");
-        startActivityForResult(originActivity, intent, Constants.REQUESTCODE_PICK_IMAGE, null);
+        startActivityForResult(originActivity, intent, REQUESTCODE_PICK_IMAGE, null);
+    }
+
+    public static void setLoginPreferences(Context context, String username, String pw) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREFERENCES_FILE_NAME, MODE_PRIVATE).edit();
+        editor.putString(PREFNAME_USERNAME, username);
+        editor.putString(PREFNAME_PASSWORD, pw);
+        editor.apply();
     }
 
 }
